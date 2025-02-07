ea41e7e514874e13db67aa75435bea8f7b73e9f1
smit <smitghedia992@gmail.com>
2025-02-07 17:48:54 +0530

    init

---
+++ src/main/java/com/grownited/ECommerceFmcgApplication.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFmcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceFmcgApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/grownited/controller/SessionController.java
@@ -0,0 +1,18 @@
+package com.grownited.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SessionController {
+
+	@GetMapping("signup")
+	public String signup() {
+		return "Signup";
+	}
+	
+	@GetMapping("login")
+	public String login() {
+		return "Login";
+	}
+}
